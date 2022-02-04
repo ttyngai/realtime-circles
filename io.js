7324d672e6f81a18430bd7cc9665e535fe288035
ttyngai <ttyngai@gmail.com>
2022-02-04 14:43:51 -0800

    Finish full circle

--- io.js
+++ io.js
@@ -1,10 +1,18 @@
 // const socketIO = require('socket.io')
 // const ioServer = socketIO()
 
-// io is our WebSocket.io server
 const io = require('socket.io')();
 
 // Listen for new connections from clients (called sockets)
 io.on('connection', function (socket) {
-  console.log('Client connected to Socket.io');
+  console.log('Client connected to socket.io');
+  socket.on('add-circle', function (data) {
+    io.emit('add-circle', data);
+  });
+  // clear-page
+  socket.on('clear-page', function (data) {
+    io.emit('clear-page');
+  });
 });
+
+module.exports = io;
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,29 @@
+const socket = io();
 const circles = document.getElementById('circles');
+
+socket.on('add-circle', function (data) {
+  addCircle(data);
+});
+
+socket.on('clear-page', function () {
+  circles.innerHTML = '';
+});
+
 let initials = '';
 
 // Listen for clicks anywhere in the "section"
-circles.addEventListener('click', function(evt) {
-  addCircle(evt.clientX, evt.clientY, randomBetween(10,125), getRandomRGBA());
+circles.addEventListener('click', function (evt) {
+  socket.emit('add-circle', {
+    initials,
+    x: evt.clientX,
+    y: evt.clientY,
+    dia: randomBetween(20, 150),
+    rgba: getRandomRGBA(),
+  });
 });
 
-document.querySelector('button').addEventListener('click', function() {
-  circles.innerHTML = '';
+document.querySelector('button').addEventListener('click', function () {
+  socket.emit('clear-page');
 });
 
 do {
@@ -15,11 +31,11 @@ do {
 } while (initials.length < 2 || initials.length > 3);
 
 function getInitials() {
-  const input = prompt("Please enter your initials");
+  const input = prompt('Please enter your initials');
   return input ? input.toUpperCase() : '';
 }
 
-function addCircle(x, y, dia, rgba) {
+function addCircle({ initials, x, y, dia, rgba }) {
   const el = document.createElement('div');
   el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
   el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
@@ -38,5 +54,8 @@ function randomBetween(min, max) {
 }
 
 function getRandomRGBA() {
-  return `rgba(${randomBetween(0, 255)}, ${randomBetween(0, 255)}, ${randomBetween(0, 255)}, ${randomBetween(2, 10) / 10})`;
+  return `rgba(${randomBetween(0, 255)}, ${randomBetween(
+    0,
+    255
+  )}, ${randomBetween(0, 255)}, ${randomBetween(2, 10) / 10})`;
 }
